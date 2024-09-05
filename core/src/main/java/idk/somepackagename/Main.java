2fe6d8df0973bd74b5280cc458fffd65cdf9685a
robert <rtp432@gmail.com>
2024-09-04 21:26:19 -0400

    first draw

--- core/src/main/java/idk/somepackagename/Main.java
+++ core/src/main/java/idk/somepackagename/Main.java
@@ -3,11 +3,21 @@ package idk.somepackagename;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. Listens to user input. */
 public class Main extends InputAdapter implements ApplicationListener {
+    
+    public Texture sandTexture;
+    public SpriteBatch spriteBatch;
     @Override
     public void create() {
+        spriteBatch = new SpriteBatch();
+        sandTexture = new Texture(Gdx.files.internal("Untitled.png"));
         Gdx.input.setInputProcessor(this);
     }
 
@@ -17,6 +27,10 @@ public class Main extends InputAdapter implements ApplicationListener {
 
     @Override
     public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        spriteBatch.begin();
+        spriteBatch.draw(sandTexture, 0, 0);
+        spriteBatch.end();
     }
 
     @Override
@@ -29,6 +43,8 @@ public class Main extends InputAdapter implements ApplicationListener {
 
     @Override
     public void dispose() {
+        sandTexture.dispose();
+        spriteBatch.dispose();
     }
 
     // Note: you can override methods from InputAdapter API to handle user's input.
