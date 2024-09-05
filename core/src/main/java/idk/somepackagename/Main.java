6a79697c32c347d05e01f3f6bd8622eba434a799
robert <rtp432@gmail.com>
2024-09-04 22:02:50 -0400

    im using texture atlas now

--- core/src/main/java/idk/somepackagename/Main.java
+++ core/src/main/java/idk/somepackagename/Main.java
@@ -4,32 +4,38 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. Listens to user input. */
 public class Main extends InputAdapter implements ApplicationListener {
     
-    public Texture sandTexture;
     public SpriteBatch spriteBatch;
+    public Sprite textureSprite;
+
+    private TextureAtlas atlas;
     @Override
     public void create() {
+        atlas = new TextureAtlas(Gdx.files.internal("atlas/textures.atlas"));
         spriteBatch = new SpriteBatch();
-        sandTexture = new Texture(Gdx.files.internal("Untitled.png"));
+        textureSprite = new Sprite(atlas.findRegion("sand"));
+        textureSprite.setBounds(0,0,200,200);
         Gdx.input.setInputProcessor(this);
     }
 
     @Override
     public void resize(final int width, final int height) {
+        spriteBatch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
     }
 
     @Override
     public void render() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         spriteBatch.begin();
-        spriteBatch.draw(sandTexture, 0, 0);
+        textureSprite.draw(spriteBatch);
         spriteBatch.end();
     }
 
@@ -43,7 +49,6 @@ public class Main extends InputAdapter implements ApplicationListener {
 
     @Override
     public void dispose() {
-        sandTexture.dispose();
         spriteBatch.dispose();
     }
 
