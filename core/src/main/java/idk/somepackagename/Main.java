1584052fddae721332a839c6779922953f627c2c
robert <rtp432@gmail.com>
2024-09-05 21:13:42 -0400

    AMBIENT LIGHT HELL YEAH

--- core/src/main/java/idk/somepackagename/Main.java
+++ core/src/main/java/idk/somepackagename/Main.java
@@ -6,44 +6,53 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. Listens to user input. */
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
+ * platforms. Listens to user input.
+ */
 public class Main implements ApplicationListener {
-	public PerspectiveCamera cam;
+    public Environment environment;
+    public PerspectiveCamera cam;
     public ModelBatch modelBatch;
-	public Model model;
-	public ModelInstance instance;
+    public Model model;
+    public ModelInstance instance;
 
-	@Override
-	public void create () {
+    @Override
+    public void create() {
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
         modelBatch = new ModelBatch();
 
-        
-		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(10f, 10f, 10f);
-		cam.lookAt(0,0,0);
-		cam.near = 1f;
-		cam.far = 300f;
-		cam.update();
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(10f, 10f, 10f);
+        cam.lookAt(0, 0, 0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
 
-		ModelBuilder modelBuilder = new ModelBuilder();
-		model = modelBuilder.createBox(5f, 5f, 5f, 
-			new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-			Usage.Position | Usage.Normal);
-		instance = new ModelInstance(model);
-	}
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f,
+                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+                Usage.Position | Usage.Normal);
+        instance = new ModelInstance(model);
+    }
 
-	@Override
-	public void dispose () {
-		model.dispose();
+    @Override
+    public void dispose() {
+        model.dispose();
         modelBatch.dispose();
-	}
+    }
 
     @Override
     public void resize(int width, int height) {
@@ -52,11 +61,11 @@ public class Main implements ApplicationListener {
     @Override
     public void render() {
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-		modelBatch.begin(cam);
-		modelBatch.render(instance);
-		modelBatch.end();
+        modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
     }
 
     @Override
@@ -67,6 +76,5 @@ public class Main implements ApplicationListener {
     public void resume() {
     }
 
-    
     // Note: you can override methods from InputAdapter API to handle user's input.
 }
