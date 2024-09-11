d01fad49b840dc37ab7f8aab4d3e639ca44d4f32
robert <rtp432@gmail.com>
2024-09-10 22:31:52 -0400

    ground class + getting ready to revamp player for FPS movement

--- core/src/main/java/idk/somepackagename/Main.java
+++ core/src/main/java/idk/somepackagename/Main.java
@@ -3,24 +3,18 @@ package idk.somepackagename;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Model;
-import com.badlogic.gdx.graphics.g3d.ModelInstance;
 
 /**
  * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
  * platforms. Listens to user input.
  */
 public class Main implements ApplicationListener {
-    public AssetManager assets;
-    public Environment environment;
-    public boolean loading;
     public static uiRenderer UI;
     Player plr;
     customRenderer renderer;
     PlayerInput plrinput;
+    Ground ground;
 
     @Override
     public void create() {
@@ -30,31 +24,19 @@ public class Main implements ApplicationListener {
         UI = new uiRenderer();
         UI.create();
 
-        assets = new AssetManager();
-        assets.load("ship.g3db", Model.class);
-        loading = true;
-
         renderer = new customRenderer();
 
         plr = new Player(renderer);
         plrinput = new PlayerInput(renderer);
         Gdx.input.setInputProcessor(plrinput);
-    }
 
-    private void doneLoading() {
-        Model ship = assets.get("ship.g3db", Model.class);
-        ModelInstance shipInstance = new ModelInstance(ship);
-        ModelInstance shipInstance2 = new ModelInstance(ship);
-        shipInstance2.transform.setTranslation(1, 0, 1);
-        renderer.add(shipInstance);
-        renderer.add(shipInstance2);
-        loading = false;
+        ground = new Ground(100f, 100f, renderer);
     }
 
     @Override
     public void dispose() {
-        assets.dispose();
         UI.dispose();
+        ground.dispose();
     }
 
     @Override
@@ -64,8 +46,6 @@ public class Main implements ApplicationListener {
 
     @Override
     public void render() {
-        if (loading && assets.update())
-            doneLoading();
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
--- core/src/main/java/idk/somepackagename/ground.java
+++ core/src/main/java/idk/somepackagename/ground.java
@@ -1,5 +1,35 @@
 package idk.somepackagename;
 
-public class ground {
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.g3d.Material;
 
+public class Ground {
+    private Model model;
+    private ModelInstance instance;
+
+    public Ground(float width, float depth, customRenderer renderer) {
+        Material material = new Material(
+                ColorAttribute.createDiffuse(Color.GRAY),
+                ColorAttribute.createFog(Color.BLACK));
+
+        ModelBuilder MB = new ModelBuilder();
+        model = MB.createBox(width, 1f, depth, material,
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        instance = new ModelInstance(model);
+
+        renderer.add(instance);
+    }
+
+    public ModelInstance getInstance() {
+        return instance;
+    }
+
+    public void dispose() {
+        model.dispose();
+    }
 }
